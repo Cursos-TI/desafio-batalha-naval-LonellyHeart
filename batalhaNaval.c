a94f4087dc997d9e7bb9596e625938cd3aaf4933
Matheus Ricci <69773156+LonellyHeart@users.noreply.github.com>
2026-03-25 20:34:36 -0300

    Nivel Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,84 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+int main(){
+    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // Na parte superior das colunas, ficara as letras, para facilitar que coluna esta olhando
+    char linha[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; // Na parte esquerda das linhas, ficara os numeros, para facilitar que linha esta olhando
+    int tabuleiro[10][10]; // Criação do tabuleiro
+    int navHorizontal[3] = {3,3,3}, navVertical[3] = {3,3,3}; // Criação dos navios
+    int index = 0; // Para mostrar as colunas antes do codigo de FOR
+    int linhaH, colunaH, linhaV, colunaV; //Declaração das variaveis que vão controlar a inclusão dos navios
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    //CRIANDO O TABULEIRO, COLOCANDO TODOS OS VALORES DA MATRIZ COMO 0;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    //MOSTRANDO O TABULEIRO ZERADO
+    printf("##########   TABULEIRO DA BATALHA NAVAL   ##########\n\t"); // Texto de apresentação
+    do{
+        printf("%c\t", colunas[index++]); // Imprimindo toda a Array colunas, para que fique na parte superior do código, utilizando o (index++) ele faz o incremento após mostrar o valor para seguir o looping
+    } while(index < 10); 
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    for(int i = 0; i < 10; i++){ // Mostrando agora o tabuleiro de fato
+        printf("\n%c\t" ,linha[i]); // No inicio da tabela vai mostrar o numero da linha "0-9"
+        for(int j = 0; j < 10; j++){
+            printf("%d\t" ,tabuleiro[i][j]); // Aqui que vai ser mostrado os valores que estão dentro do tabuleiro, nesse caso, vazio
+        }
+    }
+
+    //COLOCANDO OS NAVIOS
+    linhaH = 5, colunaH = 1; // Essas coodernadas vão controlar a inclusão do navio da Horizontal (Vertical fixa, Coluna Variavel)
+    linhaV = 8, colunaV = 9; // Essas coodernadas vão controlar a inclusão do navio da Vertical (Vertical Variavel, Coluna Fixa)
+
+    // Controle de erro, para caso, o valor dado no campos, irão sobrepor a matriz dada, ou seja, o navio não cabe no espaço que foi dado a ele
+    if(colunaH + 3 > 10){
+        printf("\nO Navio não cabe no espaço que foi alocado para ele, altere a posiçaõ da Coluna inicial e tente novamente\n");
+        return 0;
+    }
+
+    if(linhaV + 3 > 10){
+        printf("\nO Navio não cabe no espaço que foi alocado para ele, altere a posição da Linha inicial e tente novamente\n");
+        return 0;
+    }
+
+
+    for(int i = 0; i < 3; i++){
+        if(tabuleiro[linhaH][colunaH+i] == 0){  // inclusão do navio horizontal
+            tabuleiro[linhaH][colunaH + i] = navHorizontal[i];
+        }
+            else{ // Mensagemd e erro, para caso tente colocar o navio, onde ja tem um navio
+                printf("\nEsta havendo uma sobreposição dos navios, corriga e rode o progama novamente...\n");
+                printf("\nFechando..\n");
+                return 0;
+            }
+
+        if(tabuleiro[linhaV+i][colunaV] == 0){  // inclusão do navio horizontal
+            tabuleiro[linhaV+i][colunaV] = navVertical[i];
+        }
+            else{ // Mensagemd e erro, para caso tente colocar o navio, onde ja tem um navio
+                printf("Esta havendo uma sobreposição dos navios, corriga e rode o progama novamente...\n");
+                printf("Fechando..\n");
+                return 0;
+            }                        
+    }
+
+    //MOSTRANDO O TABULEIRO ATUALIZADO COM OS NAVIOS
+    printf("\n##########   TABULEIRO DA BATALHA NAVAL   ##########\n\t"); // Texto de apresentação
+    do{
+        printf("%c\t", colunas[index++]); // Imprimindo toda a Array colunas, para que fique na parte superior do código, utilizando o (index++) ele faz o incremento após mostrar o valor para seguir o looping
+    } while(index < 10); 
+
+
+    for(int i = 0; i < 10; i++){ // Mostrando agora o tabuleiro de fato
+        printf("\n%c\t" ,linha[i]); // No inicio da tabela vai mostrar o numero da linha "0-9"
+        for(int j = 0; j < 10; j++){
+            printf("%d\t" ,tabuleiro[i][j]); // Aqui que vai ser mostrado os valores que estão dentro do tabuleiro, nesse caso, vazio
+        }
+    }
 
     return 0;
 }
